effd710c0d53db6a1a42c1ff3608ff8e5cb2bc7e
PaolaHaro <valdiviadiazstephania@gmail.com>
2023-03-31 12:32:00 -0600

    PythonBasico.py

--- PythonBasico.py
+++ PythonBasico.py
@@ -236,4 +236,86 @@ print(ss)
 #=======================
 #  Uso de diccionarios
 #=======================
+capitals = {"USA":"Washington D.C.","France":"Paris","India":"New Delhi"}
+print(capitals)
+
+#================
+#  llave:valor
+#================
+# diccionario vacío
+d ={}
+
+# Llave entera, valor string
+numNames={1:"One", 2:"Two", 3:"Three"}
+
+# Llave real, valor estring
+decNames={1.5:"One and Half", 2.5:"Two and Half", 3.5:"Three and Half"}
+
+#Llave tupla, valor string
+items={("Parker", "Reynolds", "Camlin"): "pen", ("LG", "Whirlpool", "Samsumng"): "Refrigrator"}
+
+#Llave string, valor int
+romanNums = {'I':1, 'II':2, 'II':3, 'IV':4, 'V':5}
+print(romanNums)
+print(romanNums["I"])
+
+print(capitals.get("India"))
+print(capitals.get("india"))
+
+# Reportar llave y valor
+for k in capitals:
+    print("Key = " + k + ", Value = " + capitals[k])
+
+# Nuevo dato para el diccionario
+capitals["Mexico"] = "CDMX"
+print(capitals)
+
+# Borrar dato del diccionario
+del capitals["Mexico"]
+print(capitals)
+
+# Borrar todo el diccionario
+del capitals
+
+# Reportar llaves
+print(romanNums.keys())
+
+# Reportar valores
+print(romanNums.values())
+
+# Juicio de llave (está o no está la llave en el diccionario)
+print("I" in romanNums)
+print("X" in romanNums)
+print("XX" not in romanNums)
+
+
+
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#================================================
+#  Listas
+#  Las listas pueden ser de objetos diferentes
+#================================================
+miprimeralista = []  # Lista vacía
+print(miprimeralista)
+
+#=====================
+#  Llenado de lista
+#=====================
+miprimeralista = [1, "Javier", 1.34, "Bosco", "Angel", "Abigail", TRue]
+print(miprimeralista)
+
+#========================================
+#  list: hacer unalista
+#  range(i,j): secuencia de i hasta j-1
+#========================================
+nums = list(range(1,61))
+
+for i in nums:
+    print(i)
+
+#========================================
+#  Incluir nuevos elementos de la lista
+#========================================
+
 
