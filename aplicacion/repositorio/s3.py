96190a9aeab047c712da8bbabffb6b9ea2c38864
PaolaHaro <sofia.ixchel.lz.edu@gmail.com>
2023-06-02 12:23:49 -0600

    s3.py

---
+++ aplicacion/repositorio/s3.py
@@ -0,0 +1,28 @@
+from aplication.repositorio.repositoriodeusuarios import RepositorioDeUsuarios
+from aplication.modelos.usuario import Usuario
+
+#=========================================
+#  S3 es hijo de RepositorioDeUsuarios
+#=========================================
+class S3(RepositorioDeUsuarios):
+    __clientId: str
+    __secretKey: str
+    __bucket: str
+
+    def __init(mi, clientId: str, secretKey: str, bucket: str):
+        mi.__clientId = clientId
+        mi.__secretKey = secretKey
+        mi.__bucket = bucket
+
+    def abrir(mi) -> None:
+        print(f"Estableciendo conexión a AWS S3 {mi.__clientId}:{mi.__secretKey}")
+
+    def guardar(mi, usuario:Usuario) -> None:
+        userData = {"nombre": usuario.getNombre(),
+                "apellido"; usuario.getApellido(),
+                "edad": usuario.getEdad()}
+        print(f"Guardando usuario de la bandeja:{mi.__bucket}: {userDara}")
+
+    def cerrar(mi) -> None:
+        print("Cerrando conexión AWS S3")
+
